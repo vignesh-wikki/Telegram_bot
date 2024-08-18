192dc4f0f75073dd69eacba84e4fd74f6c365fec
vignesh-wikki <vignesha445@gmail.com>
2024-08-18 22:20:22 +0530

    Telegram_bot project files added

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ require("dotenv").config();
 const connectDB = require("./db/db");
 connectDB();
 
-const startBot = require('./bot/botStart');
+const startBot = require("./bot/botStart");
 startBot();
 
+app.get("/", async (req, res) => {res.send("telegram bot")});
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
